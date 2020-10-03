0cf837e0824caf2c1b2ff4d84da23ae157c8e91c
Glen Adamson Gmail <glenadamsongmail@glens-air.home>
2020-10-03 20:14:34 +0100

    setting up dummy data, redux and initial reducer

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import productReducer from "./store/reducers/products"
+
+const rootReducer = combineReducers({
+  products: productReducer
+});
+
+const store = createStore(rootReducer);
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>A new app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View><Text>Home</Text></View>
+    </Provider>
   );
 }
 
---
+++ screens/shop/ProductsOverviewScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native"
+
+const ProductOverviewScreen = () => {
+    return (
+        <View></View>
+    )
+}
+
+export default ProductOverviewScreen;
