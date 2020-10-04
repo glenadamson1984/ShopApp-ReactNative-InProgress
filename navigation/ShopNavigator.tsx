100e22ad32ca746a0d87f5abdfcd5f633180ea7e
Glen Adamson Gmail <glenadamsongmail@glens-air.home>
2020-10-04 09:14:31 +0100

    changing nav

--- navigation/ShopNavigator.tsx
+++ navigation/ShopNavigator.tsx
@@ -1,23 +1,24 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import ProductOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from "react-navigation-stack";
+import { Platform } from 'react-native';
+
+import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
+// import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import Colors from '../constants/Colors';
-import {Platform } from "react-native";
 
-const ProductsNavigator = createStackNavigator();
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverview: ProductsOverviewScreen,
+    // ProductDetail: ProductDetailScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+    }
+  }
+);
 
-const ShopNavigator = () => {
-    return (
-        <ProductsNavigator.Navigator screenOption={{
-            headerStyle: {
-                backgroundColor: Platform.OS === "android" ? Colors.primary : ""
-            },
-            headerTintColor: Platform.OS === "android" ? "white" : Colors.primary
-        }}>
-          <ProductsNavigator.Screen name="ProductOverview" component={ProductOverviewScreen} options={{
-          title: 'All Products',
-        }}/>
-        </ProductsNavigator.Navigator>
-    );
-}
-
-export default ShopNavigator;
+export default createAppContainer(ProductsNavigator);
